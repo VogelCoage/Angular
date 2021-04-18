543796c33b6f1125ed1e6c430a690629c0bb024c
VogelCoage <bladburn@gmail.com>
2021-04-18 15:43:32 -0500

    Tercer commit - v0.1 - proveedor ernesto visible

--- src/app/proveedores/proveedores.component.ts
+++ src/app/proveedores/proveedores.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Proveedor } from '../Proveedor';
 
 @Component({
   selector: 'app-proveedores',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProveedoresComponent implements OnInit {
 
-  proveedores = "Ernesto";
+  proveedor: Proveedor = {
+    id: 1,
+    name: 'Ernesto'
+  };
 
   constructor() { }
 

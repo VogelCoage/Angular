9ee1fb117c392a4e02e442d101c732c59bce95e2
VogelCoage <bladburn@gmail.com>
2021-04-18 14:47:04 -0500

    Segundo commit - v0.1 - pantalla blanca

---
+++ src/app/proveedores/proveedores.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-proveedores',
+  templateUrl: './proveedores.component.html',
+  styleUrls: ['./proveedores.component.css']
+})
+export class ProveedoresComponent implements OnInit {
+
+  proveedores = "Ernesto";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
